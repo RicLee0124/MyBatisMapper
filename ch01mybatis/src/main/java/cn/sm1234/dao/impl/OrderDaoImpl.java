cff8e58034b870956a673001366f48eb7d1e6f35
ric <1162220392@qq.com>
2018-06-30 15:09:58 +0800

    first commit

---
+++ ch01mybatis/src/main/java/cn/sm1234/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,19 @@
+package cn.sm1234.dao.impl;
+
+import cn.sm1234.dao.OrderDao;
+import cn.sm1234.domain.Order;
+import cn.sm1234.domain.OrderUser;
+
+import java.util.List;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public List<OrderUser> queryOrderUser() {
+        return null;
+    }
+
+    @Override
+    public List<Order> queryOrderUserResultMap() {
+        return null;
+    }
+}
---
+++ ch01mybatis/src/main/java/cn/sm1234/dao/impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package cn.sm1234.dao.impl;
+
+import cn.sm1234.dao.UserDao;
+import cn.sm1234.domain.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public List<User> queryUserOrder() {
+        return null;
+    }
+}
